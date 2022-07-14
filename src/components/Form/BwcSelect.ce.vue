3bfc2d50466ce1d05e88642d8517a9f85b91c381
leicaflorian <florian.leica@gmail.com>
2022-07-14 09:22:45 +0200

    Adds comments to BwcSelect and adds clearSelection Button

--- src/components/Form/BwcSelect.ce.vue
+++ src/components/Form/BwcSelect.ce.vue
@@ -1,30 +1,42 @@
 <template>
   <div>
-    <label :for="elName" class="form-label"
+    <!-- Form Label if not floating -->
+    <label :for="controlId" class="form-label"
            v-if="label && !floatingLabel">
       {{ label }}
     </label>
 
     <div class="dropdown" :class="{'form-floating': floatingLabel}">
+      <!-- Visible part where the user will click to open -->
       <button class="form-select"
-              :class="btnClasses"
+              :class="controlClasses"
               type="button"
-              :id="elName"
+              :id="controlId"
               aria-expanded="false"
               :placeholder="label"
               @click="show = !show">
-        {{ selectionAsText }}
+        <div class="content-wrapper">
+          <div class="selection-text">
+            {{ selectionAsText }}
+          </div>
+
+          <!-- Button for clearing the selection -->
+          <button class="btn-close" @click.stop="clearSelection" v-if="clearable"></button>
+        </div>
       </button>
 
+      <!-- Invalid feedback text -->
       <div class="invalid-feedback" role="alert" v-if="errorMessage">
-        <strong>{{ errorMessage }}</strong>
+        {{ errorMessage }}
       </div>
 
-      <label :for="elName" v-if="label && floatingLabel">
+      <!-- Form Label if floating -->
+      <label :for="controlId" v-if="label && floatingLabel">
         {{ label }}
       </label>
 
-      <ul class="dropdown-menu" :class="{show: show}" :aria-labelledby="elName">
+      <!-- Dropdown list -->
+      <ul class="dropdown-menu" :class="{show: show}" :aria-labelledby="controlId">
         <template v-if="multiple">
           <li>
             <button class="dropdown-item" type="button" data-dd-action="none">Nessuna</button>
@@ -47,7 +59,7 @@
                    :id="getOptionId(option)"
                    :name="getOptionName()"
                    :value="option[optionValueKey]"
-                   :checked="isActive(option)"
+                   :checked="optionIsActive(option)"
                    @change="onOptionChange(option)"
             >
             <label class="form-check-label" :for="`${getOptionName()}_${option[optionValueKey]}_option`">
@@ -78,21 +90,47 @@ export default defineComponent({
     options: [String, Array],
     optionLabelKey: { type: String, default: 'text' },
     optionValueKey: { type: String, default: 'value' },
-    showControlEl: Boolean
+    showControlEl: Boolean,
+    clearable: Boolean
   },
   setup (props, context) {
     const show = ref(false)
-    const selectedOptions = computed(() => {
-      const options = []
 
-      optionsList.value.forEach((el) => {
-        if (el[props.optionValueKey] === props.value) {
-          options.push(el)
-        }
-      })
+    /**
+     * Returns necessary classes for the control element
+     */
+    const controlClasses = computed(() => {
+      const toReturn: any = {}
 
-      return options
+      // Add is-invalid in case of an error message
+      if (props.errorMessage) {
+        toReturn['is-invalid'] = true
+      }
+
+      // Set the size of the element
+      if (props.size) {
+        toReturn['form-select-' + props.size] = true
+      }
+
+      return toReturn
     })
+
+    /**
+     * Generate the id for the control element
+     */
+    const controlId = computed(() => {
+      let name = props.name
+
+      if (!name) {
+        name = 'form'
+      }
+
+      return [name, 'Select']
+    })
+
+    /**
+     * Generate the options list.
+     */
     const optionsList: ComputedRef<any[]> = computed(() => {
       let toReturn: any
 
@@ -108,35 +146,39 @@ export default defineComponent({
 
       return toReturn ?? []
     })
-    const btnClasses = computed(() => {
-      const toReturn: any = {
-        'is-invalid': props.errorMessage
-      }
 
-      if (props.size) {
-        toReturn['form-select-' + props.size] = true
-      }
+    /**
+     * Return a list of selected options.
+     */
+    const selectedOptions = computed(() => {
+      const options = []
+      const valueList: any[] = props.value instanceof Array ? props.value : [props.value]
 
-      return toReturn
+      optionsList.value.forEach((el) => {
+        if (valueList.includes(el[props.optionValueKey])) {
+          options.push(el)
+        }
+      })
+
+      return options
     })
-    const elName = computed(() => {
-      let name = props.name
 
-      if (!name) {
-        name = 'form'
-      }
-
-      return [name, 'Select']
-    })
+    /**
+     * Return the text representation of the selected options.
+     */
     const selectionAsText = computed(() => {
       return selectedOptions.value.reduce((acc, curr) => {
         acc.push(curr[props.optionValueKey])
 
         return acc
-      }, []).join(' ')
+      }, []).join(', ')
     })
 
-    function isActive (option) {
+    /**
+     * Indicate if the option must be active
+     * @param option
+     */
+    function optionIsActive (option) {
       if (props.value instanceof Array) {
         return props.value.includes(option[props.optionValueKey])
       } else {
@@ -144,41 +186,69 @@ export default defineComponent({
       }
     }
 
+    /**
+     * Return the name of dropdown checkbox.
+     */
     function getOptionName () {
       return props.multiple ? props.name + '[]' : props.name
     }
 
+    /**
+     * Return the id of an option.
+     */
     function getOptionId (option) {
       return getOptionName() + `_${option[props.optionValueKey]}_option`
     }
 
+    /**
+     * Handle the change of an option.
+     * @param option
+     */
     function onOptionChange (option) {
       if (!props.multiple) {
         show.value = false
       }
 
+      const changedOption = option[props.optionValueKey]
+      const newSelection: any[] = selectedOptions.value
+
+      /*
+        * If the option is already selected, remove it from the selection.
+        * otherwise add it to the selection
+        *
+        * Cycle through the selection and push in the newSelection array only the right option,
+        * handling the case where the option is already selected, then remove it from the selection.
+       */
+
       context.emit('update:modelValue', {
-        value: option[props.optionValueKey],
+        value: changedOption,
         options: option
       })
 
-      context.emit('change', option[props.optionValueKey])
+      context.emit('change', changedOption)
+    }
+
+    /**
+     * Clear the selection of the dropdown.
+     */
+    function clearSelection () {
+      onOptionChange({})
     }
 
     context.expose({
-      getOptionName,
       selectedOptions
     })
 
     return {
-      optionsList,
       show,
-      btnClasses,
-      elName,
-      isActive,
+      controlClasses,
+      controlId,
+      optionsList,
       selectionAsText,
-      getOptionId,
       getOptionName,
+      getOptionId,
+      optionIsActive,
+      clearSelection,
       onOptionChange
     }
   }
@@ -198,6 +268,7 @@ export default defineComponent({
 @import "bootstrap/scss/forms/floating-labels";
 @import "bootstrap/scss/forms/validation";
 @import "bootstrap/scss/dropdown";
+@import "bootstrap/scss/close";
 
 * {
   box-sizing: border-box;
@@ -208,8 +279,25 @@ export default defineComponent({
   z-index: 99;
 }
 
-.hidden-control{
+.hidden-control {
   padding-left: 0;
+}
+
+.form-select {
+  position: relative;
+
+  .content-wrapper {
+    display: flex;
+    align-items: center;
+
+    .selection-text {
+      flex: 1;
+    }
+
+    .btn-close {
+
+    }
+  }
 }
 </style>
 
